e4aecea8fd1078aebb70aaae4b0bc70cca6e08f2
crossaaron <atcbcrossx@gmail.com>
2019-02-05 18:53:03 -0800

    fix test confirmString

--- confirmString/test/confirmString.js
+++ confirmString/test/confirmString.js
@@ -3,15 +3,15 @@ let confirmString = require('../index.js');
 
 describe("Confirm String should take and array with 2 strings.  It will check the first string to evaluate if the letters in the second string are in it", function () {
 
-    it('confirmString("Hello", "hey") should return false', function() {
-        expect(confirmString('Hello', 'hey')).to.equal(false);
+    it('confirmString(["Hello", "hey"]) should return false', function() {
+        expect(confirmString(['Hello', 'hey'])).to.equal(false);
     });
 
-    it('confirmString("Super", "repus") should return true', function() {
-        expect(confirmString('Super', 'repus')).to.equal(true);
+    it('confirmString(["Super", "repus"]) should return true', function() {
+        expect(confirmString(['Super', 'repus'])).to.equal(true);
     });
 
-    it('confirmString("qwertyipo6457", "pw6y") should return true', function() {
-        expect(confirmString("qwertyipo6457", "pw6y").to.equal(true));
-    })
+    it('confirmString(["qwertyipo6457", "pw6y"]) should return true', function() {
+        expect(confirmString(["qwertyipo6457", "pw6y"])).to.equal(true);
+    });
 });
