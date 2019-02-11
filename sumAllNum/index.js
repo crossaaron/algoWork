f423c728d793be1359e56f8345c0534dcbc5c3ec
crossaaron <atcbcrossx@gmail.com>
2019-02-11 10:35:33 -0800

    numSum update

--- sumAllNum/index.js
+++ sumAllNum/index.js
@@ -4,7 +4,10 @@ function sumAll(arr) {
     let largeNum = (arr[0] - arr[1]) < 0 ? arr[1] : arr[0];
     let smallNum = (arr[0] - arr[1]) < 0 ? arr[0] : arr[1];
 
-    let diff = largeNum - smallNum
+    //alternative solution
+    //Math.max(arr[0], arr[1]);
+    //Math.min(arr[0], arr[1]);
+
     //initial sum of numbers
     let initNumSum = arr[0] + arr[1]
     let betweenArr = [];
